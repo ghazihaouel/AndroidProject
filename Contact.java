57b332167891051cc6199fa3fd1090e550e5c391
ghazihaouel <ghazi.glult@gmail.com>
2017-03-09 02:08:34 +0100

    Create Contact.java

---
+++ Contact.java
@@ -0,0 +1,25 @@
+package com.example.user.pizzanet;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by USER on 23/02/2017.
+ */
+
+public class Contact extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.contact, container, false);
+
+        return rootView;
+    }
+
+
+}

0c7d9dc346178d81d6378fd7f334bd7c51c9b236
ghazihaouel <ghazi.glult@gmail.com>
2017-03-09 02:10:23 +0100

    Create Compte.java

---
+++ Compte.java
@@ -0,0 +1,25 @@
+package com.example.user.pizzanet;
+
+/**
+ * Created by USER on 23/02/2017.
+ */
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Compte  extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.compte, container, false);
+
+        return rootView;
+    }
+}

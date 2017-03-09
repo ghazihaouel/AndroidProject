9a790b14741eb7927250ff16045372d8a09419fb
ghazihaouel <ghazi.glult@gmail.com>
2017-03-09 02:07:10 +0100

    Create Pizza.java

---
+++ Pizza.java
@@ -0,0 +1,30 @@
+package com.example.user.pizzanet;
+
+
+
+/**
+ * Created by USER on 23/02/2017.
+ */
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Pizza extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.pizza, container, false);
+
+        return rootView;
+    }
+
+
+}
+
+
+
